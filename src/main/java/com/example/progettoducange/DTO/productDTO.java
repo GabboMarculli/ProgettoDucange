bc3fe3e71924521557f584d7ab3f4cbfb7950665
erten <ertento@gmail.com>
2022-12-15 21:43:14 +0100

    dto

--- src/main/java/com/example/progettoducange/DTO/productDTO.java
+++ src/main/java/com/example/progettoducange/DTO/productDTO.java
@@ -1,5 +1,40 @@
 package com.example.progettoducange.DTO;
 
 public class productDTO {
+    private long id;
+    private String name;
+    private int quantity;
+    private LocalDate date;
 
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
