361317039ccf6a07a8c7c70edb00be194f6a7d10
gabri <gabrielemarcuccetti@gmail.com>
2022-12-29 17:57:21 +0100

    Aggiunto cerca user in "All users"

--- src/main/java/com/example/progettoducange/Controller/AllUsersController.java
+++ src/main/java/com/example/progettoducange/Controller/AllUsersController.java
@@ -1,7 +1,9 @@
 package com.example.progettoducange.Controller;
 
 import com.example.progettoducange.Application;
+import com.example.progettoducange.DAO.IngredientDAO;
 import com.example.progettoducange.DAO.userDAO;
+import com.example.progettoducange.DTO.IngredientDTO;
 import com.example.progettoducange.DTO.userDTO;
 import com.example.progettoducange.model.ProductInFridge;
 import com.example.progettoducange.model.RegisteredUser;
@@ -9,10 +11,7 @@ import com.example.progettoducange.model.User;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Callback;
 import org.bson.Document;
@@ -38,16 +37,18 @@ public class AllUsersController {
     public TableColumn<userDTO, LocalDate> NumberOfRecipe;
     @FXML
     public TableColumn FollowButtonColumn;
+    @FXML
+    public TextField SearchUser;
 
 
     private ObservableList<userDTO> data = FXCollections.observableArrayList();
 
-    public void initialize(){
+    public void initialize() {
         UsernameColumn.setCellValueFactory(
-                new PropertyValueFactory<userDTO,String>("name")
+                new PropertyValueFactory<userDTO, String>("name")
         );
         CountryColumn.setCellValueFactory(
-                new PropertyValueFactory<userDTO,String>("country")
+                new PropertyValueFactory<userDTO, String>("country")
         );
         NumberOfRecipe.setCellValueFactory(
                 new PropertyValueFactory<userDTO, LocalDate>("id")
@@ -56,34 +57,34 @@ public class AllUsersController {
                 new PropertyValueFactory<>(""));
 
         Callback<TableColumn<userDTO, String>, TableCell<userDTO, String>> cellFactory
-                =   new Callback<>() {
+                = new Callback<>() {
+            @Override
+            public TableCell call(final TableColumn<userDTO, String> param) {
+                final TableCell<userDTO, String> cell = new TableCell<userDTO, String>() {
+
+                    final Button btn = new Button("Follow");
+
                     @Override
-                    public TableCell call(final TableColumn<userDTO, String> param) {
-                        final TableCell<userDTO, String> cell = new TableCell<userDTO, String>() {
-
-                            final Button btn = new Button("Follow");
-
-                            @Override
-                            public void updateItem(String item, boolean empty) {
-                                super.updateItem(item, empty);
-                                if (empty) {
-                                    setGraphic(null);
-                                    setText(null);
-                                } else {
-                                    btn.setOnAction(event -> {
-                                        userDTO user = getTableView().getItems().get(getIndex());
-                                        //userDAO.follow_a_user(Integer.parseInt(Application.authenticatedUser.id),user.getId());
-                                        userDAO.follow_a_user(Application.authenticatedUser.id,user.getId());
-                                        //System.out.println(Application.authenticatedUser.getUsername()+ " FOLLOWS " + user.getUsername());
-                                    });
-                                    setGraphic(btn);
-                                    setText(null);
-                                }
-                            }
-                        };
-                        return cell;
+                    public void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                            setText(null);
+                        } else {
+                            btn.setOnAction(event -> {
+                                userDTO user = getTableView().getItems().get(getIndex());
+                                //userDAO.follow_a_user(Integer.parseInt(Application.authenticatedUser.id),user.getId());
+                                userDAO.follow_a_user(Application.authenticatedUser.id, user.getId());
+                                //System.out.println(Application.authenticatedUser.getUsername()+ " FOLLOWS " + user.getUsername());
+                            });
+                            setGraphic(btn);
+                            setText(null);
+                        }
                     }
                 };
+                return cell;
+            }
+        };
 
         FollowButtonColumn.setCellFactory(cellFactory);
         UserTable.setItems(data);
@@ -91,12 +92,13 @@ public class AllUsersController {
     }
 
     int called_times = 0;
+
     //fill the table with 20 user. By pressing the button the user will visualize
     // 20 user in plus appended at the end of the previous list of user
-    public void fillTable(){
+    public void fillTable() {
         int limit_views_user = 20;
-        ArrayList<Document> users = getListOfUser(limit_views_user,called_times);
-        for(Document us : users) {
+        ArrayList<Document> users = getListOfUser(limit_views_user, called_times);
+        for (Document us : users) {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
             userDTO newrow = new userDTO(Integer.parseInt(
                     us.get("id").toString()),
@@ -104,11 +106,31 @@ public class AllUsersController {
                     us.get("password").toString(),
                     us.get("name").toString(),
                     us.get("surname").toString(),
-                    LocalDate.parse(us.get("registrationdate").toString(),formatter),
+                    LocalDate.parse(us.get("registrationdate").toString(), formatter),
                     us.get("country").toString());
             data.add(newrow);
         }
         called_times++;
+    }
+
+    public void Search_for_user()
+    {
+            String username = SearchUser.getText();
+            if(!username.equals("")) {
+                try {
+                    String[] searched_user = userDAO.getUser(username);
+                    userDTO us = new userDTO(Long.parseLong(searched_user[0]),searched_user[1], null, searched_user[3],
+                            searched_user[4],null, searched_user[2]);
+                    if(searched_user != null)
+                    {
+                        data.clear();
+                        data.add(us);
+                        UserTable.setItems(data);
+                    }
+                } catch (Error e){
+                    System.out.println(e);
+                }
+            }
     }
 
     @FXML
