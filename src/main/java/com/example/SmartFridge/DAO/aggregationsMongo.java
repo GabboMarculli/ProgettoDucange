9816d9449a3d50e96f99ae48686a85d2126a1810
gabri <gabrielemarcuccetti@gmail.com>
2023-01-11 10:42:42 +0100

    aggregation

--- src/main/java/com/example/SmartFridge/DAO/aggregationsMongo.java
+++ src/main/java/com/example/SmartFridge/DAO/aggregationsMongo.java
@@ -12,6 +12,7 @@ import static com.mongodb.client.model.Accumulators.sum;
 import static com.mongodb.client.model.Aggregates.*;
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Projections.*;
 import static com.mongodb.client.model.Sorts.descending;
 import static java.util.Collections.sort;
 
@@ -36,13 +37,53 @@ public class aggregationsMongo {
     }
 
     public List<Document> userMostCommented(){
-        MongoCollection<Document> collection = MongoDbDriver.getRecipeCollection();
+        MongoCollection<Document> collection = MongoDbDriver.getUserCollection();
         List<Bson> query = new ArrayList<>();
 
         query.add(unwind("$reviews"));
         query.add(group("Reviews.profileID", sum("count",1)));
         query.add(sort("count"));
+        query.add(project(fields(excludeId(), include("id"), include("count"), include("$id.profile"))));
         query.add(limit(10));
+
+        List<Document> results = null;
+        try{
+            results = collection.aggregate(query).into(new ArrayList<>());
+        } catch (MongoException ex){
+            System.out.println(ex);
+        }
+
+        return results;
+    }
+
+    public List<Document> top10Ingredients(){
+        MongoCollection<Document> collection = MongoDbDriver.getRecipeCollection();
+        List<Bson> query = new ArrayList<>();
+
+        query.add(unwind("$IngredientList"));
+        query.add(group("$IngredientList", sum("count",1)));
+        query.add(sort("count"));
+        query.add(project(fields(excludeId(), include("RecipeID"), include("count"), include("$id.ingredient"))));
+        query.add(limit(10));
+
+        List<Document> results = null;
+        try{
+            results = collection.aggregate(query).into(new ArrayList<>());
+        } catch (MongoException ex){
+            System.out.println(ex);
+        }
+
+        return results;
+    }
+
+    public List<Document> ingredientsByCountry()
+    {
+        MongoCollection<Document> collection = MongoDbDriver.getUserCollection();
+        List<Bson> query = new ArrayList<>();
+
+
+
+
 
         List<Document> results = null;
         try{
