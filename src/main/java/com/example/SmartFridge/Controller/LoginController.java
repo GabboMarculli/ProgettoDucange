0f0195543cb81b3135fe22d8291dc954062083b1
erten <ertento@gmail.com>
2023-01-23 02:47:17 +0100

    aggiunti textfield e sistemato il menu a tendina di country

--- src/main/java/com/example/SmartFridge/Controller/LoginController.java
+++ src/main/java/com/example/SmartFridge/Controller/LoginController.java
@@ -11,6 +11,7 @@ import com.example.SmartFridge.DAO.UserDAO;
 import com.example.SmartFridge.model.Recipe;
 import com.example.SmartFridge.model.RegisteredUser;
 import com.mongodb.client.MongoCollection;
+import javafx.animation.FillTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventType;
@@ -22,7 +23,9 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
@@ -84,8 +87,20 @@ public class LoginController {
     private TextArea ingredienttext;
     @FXML
     private TextArea preparationtext;
+    @FXML
+    private Button previousButton;
+    @FXML
+    private Button nextButton;
 
     private int called = 0;
+
+    @FXML
+    protected void onNextClick(){
+
+    }
+
+    @FXML
+    protected void onPreviousClick(){}
 
     // Creation of methods which are activated on events in the forms
     @FXML
@@ -196,15 +211,23 @@ public class LoginController {
         Application.changeScene("HomePageAdmin");
     }
 
-    public void initialize(){
+    public void initialize() throws IOException {
         ingredienttext.setWrapText(true);
         preparationtext.setWrapText(true);
 
-        ObservableList<String> countries = MongoDbDriver.getCountry();
-        countries.add("");
-        signUpCountryTextField.setItems(countries);
+
+        signUpCountryTextField.setItems(getCountryData());
+
+/*
+        FileWriter f = new FileWriter("country.txt");
+        PrintWriter pw = new PrintWriter(f);
+        pw.println("ObservableList<RecipeDTO> countries = FXCollections.observableArrayList();");
+        for(String s : countries)
+            pw.println("countries.add(\""+s+"\");");
+        pw.close();
+*/
         int limit_views_recipe = 20;
-        List<RecipeDTO> recipes = RecipeDao.getRecipeLoginpage(limit_views_recipe,0);
+        List<RecipeDTO> recipes = RecipeDao.getRecipeLoginpage(limit_views_recipe,20);
         listview.setCellFactory(new Callback<ListView<RecipeDTO>, ListCell<RecipeDTO>>() {
             @Override
             public ListCell call(ListView<RecipeDTO> listView) {
@@ -374,5 +397,206 @@ public class LoginController {
         ((Button) mouseEvent.getSource()).setStyle(onReleased);
         Application.setMousePointer();
     }
-
+    private ObservableList getCountryData(){
+        ObservableList<String> countries = FXCollections.observableArrayList();
+        countries.add("Afghanistan");
+        countries.add("Albania");
+        countries.add("Algeria");
+        countries.add("Andorra");
+        countries.add("Angola");
+        countries.add("Antigua & Deps");
+        countries.add("Argentina");
+        countries.add("Armenia");
+        countries.add("Australia");
+        countries.add("Austria");
+        countries.add("Azerbaijan");
+        countries.add("Bahamas");
+        countries.add("Bahrain");
+        countries.add("Bangladesh");
+        countries.add("Barbados");
+        countries.add("Belarus");
+        countries.add("Belgium");
+        countries.add("Belize");
+        countries.add("Benin");
+        countries.add("Bhutan");
+        countries.add("Bolivia");
+        countries.add("Bosnia Herzegovina");
+        countries.add("Botswana");
+        countries.add("Brazil");
+        countries.add("Brunei");
+        countries.add("Bulgaria");
+        countries.add("Burkina");
+        countries.add("Burundi");
+        countries.add("Cambodia");
+        countries.add("Cameroon");
+        countries.add("Canada");
+        countries.add("Cape Verde");
+        countries.add("Central African Rep");
+        countries.add("Chad");
+        countries.add("Chile");
+        countries.add("China");
+        countries.add("Colombia");
+        countries.add("Comoros");
+        countries.add("Congo");
+        countries.add("Congo {Democratic Rep}");
+        countries.add("Costa Rica");
+        countries.add("Croatia");
+        countries.add("Cuba");
+        countries.add("Cyprus");
+        countries.add("Czech Republic");
+        countries.add("Denmark");
+        countries.add("Djibouti");
+        countries.add("Dominica");
+        countries.add("Dominican Republic");
+        countries.add("East Timor");
+        countries.add("Ecuador");
+        countries.add("Egypt");
+        countries.add("El Salvador");
+        countries.add("Equatorial Guinea");
+        countries.add("Eritrea");
+        countries.add("Estonia");
+        countries.add("Ethiopia");
+        countries.add("Fiji");
+        countries.add("Finland");
+        countries.add("France");
+        countries.add("Gabon");
+        countries.add("Gambia");
+        countries.add("Georgia");
+        countries.add("Germany");
+        countries.add("Ghana");
+        countries.add("Greece");
+        countries.add("Grenada");
+        countries.add("Guatemala");
+        countries.add("Guinea");
+        countries.add("Guinea-Bissau");
+        countries.add("Guyana");
+        countries.add("Haiti");
+        countries.add("Honduras");
+        countries.add("Hungary");
+        countries.add("Iceland");
+        countries.add("India");
+        countries.add("Indonesia");
+        countries.add("Iran");
+        countries.add("Iraq");
+        countries.add("Ireland {Republic}");
+        countries.add("Israel");
+        countries.add("Italy");
+        countries.add("Ivory Coast");
+        countries.add("Jamaica");
+        countries.add("Japan");
+        countries.add("Jordan");
+        countries.add("Kazakhstan");
+        countries.add("Kenya");
+        countries.add("Kiribati");
+        countries.add("Korea North");
+        countries.add("Korea South");
+        countries.add("Kosovo");
+        countries.add("Kuwait");
+        countries.add("Kyrgyzstan");
+        countries.add("Laos");
+        countries.add("Latvia");
+        countries.add("Lebanon");
+        countries.add("Lesotho");
+        countries.add("Liberia");
+        countries.add("Libya");
+        countries.add("Liechtenstein");
+        countries.add("Lithuania");
+        countries.add("Luxembourg");
+        countries.add("Macedonia");
+        countries.add("Madagascar");
+        countries.add("Malawi");
+        countries.add("Malaysia");
+        countries.add("Maldives");
+        countries.add("Mali");
+        countries.add("Malta");
+        countries.add("Marshall Islands");
+        countries.add("Mauritania");
+        countries.add("Mauritius");
+        countries.add("Mexico");
+        countries.add("Micronesia");
+        countries.add("Moldova");
+        countries.add("Monaco");
+        countries.add("Mongolia");
+        countries.add("Montenegro");
+        countries.add("Morocco");
+        countries.add("Mozambique");
+        countries.add("Myanmar, {Burma}");
+        countries.add("Namibia");
+        countries.add("Nauru");
+        countries.add("Nepal");
+        countries.add("Netherlands");
+        countries.add("New Zealand");
+        countries.add("Nicaragua");
+        countries.add("Niger");
+        countries.add("Nigeria");
+        countries.add("Norway");
+        countries.add("Oman");
+        countries.add("Pakistan");
+        countries.add("Palau");
+        countries.add("Panama");
+        countries.add("Papua New Guinea");
+        countries.add("Paraguay");
+        countries.add("Peru");
+        countries.add("Philippines");
+        countries.add("Poland");
+        countries.add("Portugal");
+        countries.add("Qatar");
+        countries.add("Romania");
+        countries.add("Russian Federation");
+        countries.add("Rwanda");
+        countries.add("Saint Vincent & the Grenadines");
+        countries.add("Samoa");
+        countries.add("San Marino");
+        countries.add("Sao Tome & Principe");
+        countries.add("Saudi Arabia");
+        countries.add("Senegal");
+        countries.add("Serbia");
+        countries.add("Seychelles");
+        countries.add("Sierra Leone");
+        countries.add("Singapore");
+        countries.add("Slovakia");
+        countries.add("Slovenia");
+        countries.add("Solomon Islands");
+        countries.add("Somalia");
+        countries.add("South Africa");
+        countries.add("South Sudan");
+        countries.add("Spain");
+        countries.add("Sri Lanka");
+        countries.add("St Kitts & Nevis");
+        countries.add("St Lucia");
+        countries.add("Sudan");
+        countries.add("Suriname");
+        countries.add("Swaziland");
+        countries.add("Sweden");
+        countries.add("Switzerland");
+        countries.add("Syria");
+        countries.add("Taiwan");
+        countries.add("Tajikistan");
+        countries.add("Tanzania");
+        countries.add("Thailand");
+        countries.add("Togo");
+        countries.add("Tonga");
+        countries.add("Trinidad & Tobago");
+        countries.add("Tunisia");
+        countries.add("Turkey");
+        countries.add("Turkmenistan");
+        countries.add("Tuvalu");
+        countries.add("Uganda");
+        countries.add("Ukraine");
+        countries.add("United Arab Emirates");
+        countries.add("United Kingdom");
+        countries.add("United States");
+        countries.add("Uruguay");
+        countries.add("Uzbekistan");
+        countries.add("Vanuatu");
+        countries.add("Vatican City");
+        countries.add("Venezuela");
+        countries.add("Vietnam");
+        countries.add("Yemen");
+        countries.add("Zambia");
+        countries.add("Zimbabwe");
+        countries.add("italy");
+        countries.add("");
+        return countries;
+    }
 }
