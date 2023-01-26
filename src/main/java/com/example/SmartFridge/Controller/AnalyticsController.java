7ccd6344aaccc24533784b6aff8a2584f2fadec9
erten <ertento@gmail.com>
2023-01-26 16:41:44 +0100

    finita la grafica, messo l'aggiornamento delle tabelle quando si cambia tab, rifatta una searchuser per l'admin, da vedere se funzionano queste cose perchè a me non vanno ma può darsi che non abbia aggiornato il db: 1)increment,remove,decrement recipe non funzionano a volte, nella analytics appena si apre ci sono delle scritte "campo", nelle analytics c'è un bottno showmore che non fa nulla.

--- src/main/java/com/example/SmartFridge/Controller/AnalyticsController.java
+++ src/main/java/com/example/SmartFridge/Controller/AnalyticsController.java
@@ -5,8 +5,11 @@ import com.example.SmartFridge.DAO.aggregationsMongo;
 import com.example.SmartFridge.DTO.AggregationTransportDTO;
 import com.example.SmartFridge.DTO.IngredientDTO;
 import com.example.SmartFridge.Utils.Utils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -16,7 +19,12 @@ import java.util.ArrayList;
 
 public class AnalyticsController {
     @FXML
-    private GridPane left;
+    private GridPane left3;
+    @FXML
+    private GridPane left2;
+    @FXML
+    private PieChart piechart;
+    final private ObservableList<PieChart.Data> datapie= FXCollections.observableArrayList();
     public void setClick(MouseEvent mouseEvent) {
         Utils.setClick(mouseEvent);
     }
@@ -31,6 +39,7 @@ public class AnalyticsController {
         Utils.unsetOver(mouseEvent);
     }
     public void initialize(){
+        piechart.setData(datapie);
         ingredientsByCountry();
     }
 
@@ -39,10 +48,15 @@ public class AnalyticsController {
         ArrayList<AggregationTransportDTO> result = aggregationsMongo.top10Ingredients();
         ArrayList<IngredientDTO> ingredientList = new ArrayList<>();
 
-        left.getChildren().clear();
+        left2.getChildren().clear();
+        left3.getChildren().clear();
         printAnalytics("INGREDIENT","TOTAL USAGE",0);
         int i=1;
+        datapie.clear();
+        piechart.setTitle("Top 10 most ingredients:");
         for(AggregationTransportDTO  r : result){
+            if(i < 10)
+                datapie.add(new PieChart.Data(r.getField1(),r.getField3()));
             printAnalytics(r.getField1(),String.valueOf(r.getField3()),i);
             i++;
         }
@@ -51,10 +65,15 @@ public class AnalyticsController {
         ArrayList<AggregationTransportDTO> result = aggregationsMongo.top10votedrecipe();
         ArrayList<IngredientDTO> ingredientList = new ArrayList<>();
 
-        left.getChildren().clear();
+        left2.getChildren().clear();
+        left3.getChildren().clear();
         printAnalytics("RECIPE","AVERAGE RATE",0);
         int i=1;
+        datapie.clear();
+        piechart.setTitle("Top 5 recipes:");
         for(AggregationTransportDTO  r : result){
+            if(i < 6)
+                datapie.add(new PieChart.Data(r.getField1(),r.getField3()));
             printAnalytics(r.getField1(),String.valueOf(r.getField3()),i);
             i++;
         }
@@ -65,10 +84,15 @@ public class AnalyticsController {
         ArrayList<AggregationTransportDTO> result = aggregationsMongo.userMostCommented();
         ArrayList<IngredientDTO> ingredientList = new ArrayList<>();
 
-        left.getChildren().clear();
+        left2.getChildren().clear();
+        left3.getChildren().clear();
         printAnalytics("USERNAME","TOTAL COMMENTS",0);
         int i=1;
+        datapie.clear();
+        piechart.setTitle("Top 10 most active user:");
         for(AggregationTransportDTO  r : result){
+            if(i < 10)
+                datapie.add(new PieChart.Data(r.getField1(),r.getField3()));
             printAnalytics(r.getField1(),String.valueOf(r.getField3()),i);
             i++;
         }
@@ -78,10 +102,17 @@ public class AnalyticsController {
         ArrayList<AggregationTransportDTO> result = aggregationsMongo.ingredientsByCountry();
         ArrayList<IngredientDTO> ingredientList = new ArrayList<>();
 
-        left.getChildren().clear();
+        left2.getChildren().clear();
+        left3.getChildren().clear();
         printAnalytics_2("COUNTRY","INGREDIENT","QUANTITY",0);
         int i=1;
+        //result.sort(result);
+
+        datapie.clear();
+        piechart.setTitle("");
         for(AggregationTransportDTO  r : result){
+            if(i<6)
+                datapie.add(new PieChart.Data(r.getField1(),r.getField3()));
             printAnalytics_2(r.getField1(),r.getField2(),String.valueOf(r.getField3()),i);
             i++;
         }
@@ -103,12 +134,12 @@ public class AnalyticsController {
         GridPane.setColumnIndex(label2, 2);
 
 
-        left.getChildren().add(label);
-        left.getChildren().add(labelText);
-        left.getChildren().add(label2);
-        left.setHalignment(label, HPos.LEFT);
-        left.setHalignment(labelText, HPos.LEFT);
-        left.setHalignment(label2, HPos.LEFT);
+        left3.getChildren().add(label);
+        left3.getChildren().add(labelText);
+        left3.getChildren().add(label2);
+        left3.setHalignment(label, HPos.LEFT);
+        left3.setHalignment(labelText, HPos.LEFT);
+        left3.setHalignment(label2, HPos.LEFT);
     }
 
     public void printAnalytics(String field1, String field2, int index){
@@ -123,10 +154,10 @@ public class AnalyticsController {
             GridPane.setColumnIndex(labelText, 1);
 
 
-            left.getChildren().add(label);
-            left.getChildren().add(labelText);
-            left.setHalignment(label, HPos.LEFT);
-            left.setHalignment(labelText, HPos.LEFT);
+            left2.getChildren().add(label);
+            left2.getChildren().add(labelText);
+            left2.setHalignment(label, HPos.LEFT);
+            left2.setHalignment(labelText, HPos.LEFT);
     }
 
     @FXML
